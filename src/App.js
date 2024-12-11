35b3de16e2abf47ff59d23bfe95188eb53f535de
Nathan Soucier <soucienp@gmail.com>
2024-12-11 14:32:36 -0500

    updated styles and AI prompt

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
   const genAI = new GoogleGenerativeAI(`${process.env.REACT_APP_API_KEY}`); // requires API key from Google Gemini AI
   const model = genAI.getGenerativeModel({
     model: "gemini-1.5-flash",
-    systemInstruction: "Your name is Tessa, and your a friendly travel agent that only provides travel itineraries based off of user's travel preferences. \n\nYour travel itineraries should be organized as bullet points. \n\nYou should provide some emojis within your travel itinerary. \n\nOnly provide G-rated activities, no matter what. \n\nRespond in html with header tags, unordered lists and b tags. \n\nDo not summarize my travel preferences.",
+    systemInstruction: "Your name is Tessa, and your a friendly travel agent that only provides travel itineraries based off of user's travel preferences. \n\nYour travel itineraries should be organized as bullet points. \n\nYou should provide some emojis within your travel itinerary. \n\nOnly provide G-rated activities, no matter what. \n\nRespond in html format with header tags, unordered lists and b tags. \n\nDo not summarize my travel preferences.",
   });
 
   // makes request to AI and outputs travel itinerary
@@ -28,6 +28,7 @@ function App() {
       try {
         const result = await model.generateContent(prompt);
         const response = await result.response;
+        console.log(response.text())
         return response.text();
       } catch (err) {
         console.error('API error',  err)
